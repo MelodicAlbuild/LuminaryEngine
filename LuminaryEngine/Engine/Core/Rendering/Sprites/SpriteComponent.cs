526cca85f1f7dd3aaf4950f2800e6ead0e9570ce
MelodicAlbuild <ralexdrum@gmail.com>
2025-04-02 15:31:03 -0500

    feat: basic components

--- LuminaryEngine/Engine/Core/Rendering/Sprites/SpriteComponent.cs
+++ LuminaryEngine/Engine/Core/Rendering/Sprites/SpriteComponent.cs
@@ -1,6 +1,22 @@
-﻿namespace LuminaryEngine.Engine.Core.Rendering.Sprites;
+﻿using LuminaryEngine.Engine.ECS;
+using SDL2;
 
-public class SpriteComponent
+namespace LuminaryEngine.Engine.Core.Rendering.Sprites;
+
+public class SpriteComponent : IComponent
 {
-    
+    public string TextureId { get; set; }
+    public SDL.SDL_Rect? SourceRect { get; set; }
+
+    public SpriteComponent(string textureId)
+    {
+        TextureId = textureId;
+        SourceRect = null;
+    }
+
+    public SpriteComponent(string textureId, SDL.SDL_Rect sourceRect)
+    {
+        TextureId = textureId;
+        SourceRect = sourceRect;
+    }
 }
--- LuminaryEngine/Engine/ECS/Components/TransformComponent.cs
+++ LuminaryEngine/Engine/ECS/Components/TransformComponent.cs
@@ -1,6 +1,32 @@
-﻿namespace LuminaryEngine.Engine.ECS.Components;
+﻿using System.Numerics;
 
-public class TransformComponent
+namespace LuminaryEngine.Engine.ECS.Components;
+
+public class TransformComponent : IComponent
 {
+    public Vector2 Position { get; set; }
+    public float Rotation { get; set; }
+    public Vector2 Scale { get; set; }
+
+    public TransformComponent()
+    {
+        Position = Vector2.Zero;
+        Rotation = 0f;
+        Scale = Vector2.One;
+    }
+
+    public TransformComponent(float x, float y) : this()
+    {
+        Position = new Vector2(x, y);
+    }
     
+    public TransformComponent(float x, float y, float rotation) : this(x, y)
+    {
+        Rotation = rotation;
+    }
+    
+    public TransformComponent(float x, float y, float rotation, float scaleX, float scaleY) : this(x, y, rotation)
+    {
+        Scale = new Vector2(scaleX, scaleY);
+    }
 }
