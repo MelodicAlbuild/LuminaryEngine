7a33160d2526da2fbec7d3a23ac5858d62938b32
MelodicAlbuild <ralexdrum@gmail.com>
2025-04-02 15:30:23 -0500

    feat: exceptions

---
+++ LuminaryEngine/Engine/Exceptions/UnknownEntityException.cs
@@ -0,0 +1,11 @@
+﻿namespace LuminaryEngine.Engine.Exceptions;
+
+public class UnknownEntityException : Exception
+{
+    public UnknownEntityException() : base() { }
+    public UnknownEntityException(string message) : base(message) { }
+    public UnknownEntityException(string message, System.Exception inner) : base(message, inner) { }
+
+    protected UnknownEntityException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
