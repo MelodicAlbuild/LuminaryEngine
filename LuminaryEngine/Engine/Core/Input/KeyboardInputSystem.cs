f9130a44bc48e52780c8a8841e059f42405f0d22
MelodicAlbuild <ralexdrum@gmail.com>
2025-04-02 16:43:54 -0500

    feat: inputs

--- LuminaryEngine/Engine/Core/Input/KeyboardInputSystem.cs
+++ LuminaryEngine/Engine/Core/Input/KeyboardInputSystem.cs
@@ -1,6 +1,35 @@
-﻿namespace LuminaryEngine.Engine.Core.Input;
+﻿using LuminaryEngine.Engine.ECS;
+using SDL2;
 
-public class KeyboardInputSystem
+namespace LuminaryEngine.Engine.Core.Input;
+
+public class KeyboardInputSystem : InputSystem
 {
+    public KeyboardInputSystem(World world) : base(world)
+    {
+    }
     
+    public override void HandleEvents(SDL.SDL_Event e)
+    {
+        if (e.type == SDL.SDL_EventType.SDL_KEYDOWN)
+        {
+            foreach (var entity in _world.GetEntitiesWithComponents(typeof(InputStateComponent)))
+            {
+                var inputState = entity.GetComponent<InputStateComponent>();
+                inputState.PressedKeys.Add(e.key.keysym.scancode);
+            }
+        }
+        else if (e.type == SDL.SDL_EventType.SDL_KEYUP)
+        {
+            foreach (var entity in _world.GetEntitiesWithComponents(typeof(InputStateComponent)))
+            {
+                var inputState = entity.GetComponent<InputStateComponent>();
+                inputState.PressedKeys.Remove(e.key.keysym.scancode);
+            }
+        }
+    }
+
+    public override void Update()
+    {
+    }
 }
--- LuminaryEngine/Engine/Core/Input/MouseInputSystem.cs
+++ LuminaryEngine/Engine/Core/Input/MouseInputSystem.cs
@@ -1,6 +1,42 @@
-﻿namespace LuminaryEngine.Engine.Core.Input;
+﻿using System.Numerics;
+using LuminaryEngine.Engine.ECS;
+using SDL2;
 
-public class MouseInputSystem
+namespace LuminaryEngine.Engine.Core.Input;
+
+public class MouseInputSystem : InputSystem
 {
+    public MouseInputSystem(World world) : base(world) { }
+
+    public override void HandleEvents(SDL.SDL_Event e)
+    {
+        if (e.type == SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN)
+        {
+            foreach (var entity in _world.GetEntitiesWithComponents(typeof(InputStateComponent)))
+            {
+                var inputState = entity.GetComponent<InputStateComponent>();
+                inputState.PressedMouseButtons.Add(e.button.button);
+            }
+        }
+        else if (e.type == SDL.SDL_EventType.SDL_MOUSEBUTTONUP)
+        {
+            foreach (var entity in _world.GetEntitiesWithComponents(typeof(InputStateComponent)))
+            {
+                var inputState = entity.GetComponent<InputStateComponent>();
+                inputState.PressedMouseButtons.Remove(e.button.button);
+            }
+        }
+        else if (e.type == SDL.SDL_EventType.SDL_MOUSEMOTION)
+        {
+            foreach (var entity in _world.GetEntitiesWithComponents(typeof(InputStateComponent)))
+            {
+                var inputState = entity.GetComponent<InputStateComponent>();
+                inputState.MousePosition = new Vector2(e.motion.x, e.motion.y);
+            }
+        }
+    }
     
+    public override void Update()
+    {
+    }
 }
