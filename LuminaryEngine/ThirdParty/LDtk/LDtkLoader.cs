312c82c1425c0f7599a0499bbae2b811ef0b7f58
MelodicAlbuild <ralexdrum@gmail.com>
2025-04-08 00:49:24 -0500

    feat: ldtk module

---
+++ LuminaryEngine/ThirdParty/LDtk/LDtkLoader.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+namespace LuminaryEngine.ThirdParty.LDtk
+{
+    public class LDtkLoader
+    {
+        /// <summary>
+        /// Loads an entire LDtk project from a JSON file.
+        /// </summary>
+        /// <param name="filePath">Absolute or relative path to the LDtk JSON file.</param>
+        /// <returns>An LDtkProject object populated with all available properties.</returns>
+        public static Models.LDtkProject LoadProject(string filePath)
+        {
+            if (!System.IO.File.Exists(filePath))
+            {
+                throw new System.IO.FileNotFoundException("LDtk file not found", filePath);
+            }
+
+            string json = System.IO.File.ReadAllText(filePath);
+            var project = JsonConvert.DeserializeObject<Models.LDtkProject>(json);
+
+            // If the JSON does not explicitly include layerId for each layer instance,
+            // assign it from the UID as a default.
+            if(project?.Levels != null)
+            {
+                foreach(var level in project.Levels)
+                {
+                    if(level.LayerInstances != null)
+                    {
+                        foreach(var layer in level.LayerInstances)
+                        {
+                            if(layer.LayerId == 0)  // assuming a 0 value means unassigned
+                            {
+                                layer.LayerId = layer.Uid;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return project;
+        }
+    }
+}
