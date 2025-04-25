3a2fbd69796b774ec12fb18cc06ba2ea6db05801
MelodicAlbuild <ralexdrum@gmail.com>
2025-04-25 02:28:09 -0500

    chore: stupid comments

--- LuminaryEngine/Engine/Gameplay/Crafting/Recipe.cs
+++ LuminaryEngine/Engine/Gameplay/Crafting/Recipe.cs
@@ -5,6 +5,6 @@ public class Recipe
     public string RecipeID { get; set; }
     public string ResultItemID { get; set; }
     public Dictionary<string, int> RequiredSpiritEssences { get; set; } // EssenceID : Quantity
-    public Dictionary<string, int> RequiredItems { get; set; }          // MaterialID : Quantity
+    public Dictionary<string, int> RequiredItems { get; set; }          // ItemID    : Quantity
     public string CraftingStationTag { get; set; } // Tag to identify compatible crafting stations
 }
