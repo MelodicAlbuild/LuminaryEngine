437b8e478690c8373d83b843dbd4224720de7c4e
MelodicAlbuild <ralexdrum@gmail.com>
2025-04-25 04:26:45 -0500

    feat: spirit essence

--- LuminaryEngine/Engine/Gameplay/Spirits/SpiritEssence.cs
+++ LuminaryEngine/Engine/Gameplay/Spirits/SpiritEssence.cs
@@ -1,31 +1,13 @@
 ﻿namespace LuminaryEngine.Engine.Gameplay.Spirits;
 
-public class SpiritEssenceData
+public class SpiritEssence
 {
     public string EssenceID { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string IconResourcePath { get; set; }
+    public string TextureId { get; set; }
     public SpiritType Type { get; set; }
     public SpiritTier Tier { get; set; } // Optional
 
     public Dictionary<string, float> PropertyMultipliers { get; set; } // How much it affects stats
-    // ... other essence properties
 }
-
-public enum SpiritType
-{
-    Fire,
-    Water,
-    Earth,
-    Air,
-    Light,
-    Shadow
-}
-
-public enum SpiritTier
-{
-    Basic,
-    Greater,
-    Ancient
-} // Optional
